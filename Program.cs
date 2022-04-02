8f00694008fec8096e0154ac440e59e2f782e368
Samuel Peter <you@example.com>
2022-04-02 06:39:11 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question A
+            Console.WriteLine("Hello Samuel, I am in the Backend tact of Wella Schools");
+            //Questiom B
+            Console.WriteLine("I joined this tracct because i believe i am well equipped to succede there.I have a very analytical mind, i love efficiency and implementing new solutions.\nThere is also the reason of making money also");
+        }
+    }
+}
