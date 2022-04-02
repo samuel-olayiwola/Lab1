6421d0c2add50327eff7d69a0ab37cd55249d1f2
Samuel Peter <you@example.com>
2022-04-02 11:03:26 +0000

    update

--- Program.cs
+++ Program.cs
@@ -11,27 +11,6 @@ namespace Lab1
             //Questiom B
             Console.WriteLine("I joined this tracct because i believe i am well equipped to succede there.I have a very analytical mind, i love efficiency and implementing new solutions.\nThere is also the reason of making money also");
         
-            //lab2
-
-            //declare variables
-            int age;
-            string name;
-            bool isValid;
-            short num;
-            long number;
-            float val;
-            byte data;
-            char info;
-            DateTime date;
-            decimal dec;
-
-            //implicit conversion
-            val = 5; //val is a float while 5 is an integer. It implicitly converts 5 to a foat
-            Console.WriteLine(val);
-            age = (int)5.4;//we explicitly typecast 5.4 which is a float to an int.
-            Console.WriteLine(age);
-            //
-
            
             
         
